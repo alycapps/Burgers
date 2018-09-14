562c72178bb236502b027f01065c3c885ebac08c
Aly <38168385+alycapps@users.noreply.github.com>
2018-09-14 17:03:16 -0400

    initial burger  model set up

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,22 @@
 var orm = require("orm");
 
+var burgers = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res)) {
+            cb(res);
+        }
+    },
+    insertOne: function(value, cb) {
+        orm.insertOne("burgers", burger_name, value, function(res)) {
+            cb(res);
+        }
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res)) {
+            cb(res);
+        }
+    }
+}
+
+
+module.exports = burgers;
