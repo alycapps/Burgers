e7b68fe761641aee196eceb9e95b2f5404154455
Aly <38168385+alycapps@users.noreply.github.com>
2018-09-14 14:01:57 -0400

    orm setup

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
 //Connecting node to mySQL
 //dependencies
 var mysql = require("mysql");
-
+var connection;
 //unique info needed for setting up connection
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",  
-    password: "root",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else { 
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",  
+        password: "root",
+        database: "burgers_db"
+    });
+}
 
 //actually connecting
 connection.connect(function(err) {
