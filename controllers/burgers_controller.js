07ecf5939fe5a4d320f0fe8ced1a5c0be84e877d
Aly <38168385+alycapps@users.noreply.github.com>
2018-09-14 20:05:57 -0400

    server setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+var burgers = require("burger.js");
+
+router.get("/". function(requestAnimationFrame,res) {
+    burgers.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    })
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,2 +1,27 @@
 var express = require("server");
 var bodyparser = require("body-parser");
+var path = require("path");
+var exphbs = require("express-handlebars");
+
+//set up express and heroku port option
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// serve static assets
+app.use(express.static(__dirname + './public'));
+
+//allow express to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//connect to path files
+var routes = require('./controllers/burgers_controller.js');
+app.use(routes);
+
+//start server listening
+app.listen(PORT, function() {
+    console.log("listening on PORT: " + PORT);
+});
