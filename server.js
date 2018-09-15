f99d187a38c525824d3f7661b72a92a4b7bd17de
Aly <38168385+alycapps@users.noreply.github.com>
2018-09-15 15:29:42 -0400

    created add a burger section

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var app = express();
 var PORT = process.env.PORT || 3001;
 
 // serve static assets
-app.use(express.static(__dirname + './public'));
+// app.use(express.static(__dirname + './public'));
+app.use(express.static("public"));
+
 
 //allow express to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
